d48d0d3e568f7c10bd394557b7d72cf6241e52bf
Jan Reimann <jan.reimann@staffbase.com>
2023-06-23 15:47:44 +0200

    chore(testio): retrieve payload first logging

---
+++ .github/scripts/testio/retrievePayload.ts
@@ -0,0 +1,26 @@
+import * as github from "@actions/github";
+import * as fs from "fs";
+import {Octokit} from "@octokit/rest";
+import * as core from "@actions/core";
+
+async function addComment() {
+    const commentID: number = Number(process.env.TESTIO_COMMENT_ID);
+
+    const octokit = new Octokit({
+        auth: process.env.GITHUB_TOKEN
+    });
+
+    const retrievedComment = await octokit.rest.issues.getComment({
+        repo: github.context.repo.repo,
+        owner: github.context.repo.owner,
+        comment_id: commentID
+    });
+
+    console.log("This comment has been received:");
+    console.log(retrievedComment);
+    console.log("and stringified:");
+    console.log(JSON.stringify(retrievedComment));
+
+}
+
+addComment().then();
