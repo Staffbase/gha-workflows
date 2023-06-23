0cd5f398c9497b8bebdb1f44cb006f0ebd09fc28
Jan Reimann <jan.reimann@staffbase.com>
2023-06-23 12:22:42 +0200

    chore(testio): octokit not accessible

--- .github/scripts/testio/addPRcomment.ts
+++ .github/scripts/testio/addPRcomment.ts
@@ -1,17 +1,14 @@
-import {Octokit} from "@octokit/rest";
 import * as github from "@actions/github";
 import * as fs from "fs";
-import * as path from "path";
-
-const octokit: Octokit = new Octokit({
-   auth: process.env.GITHUB_TOKEN
-});
 
 const template = fs.readFileSync(`${process.env.TESTIO_SCRIPTS_DIR}/exploratory_test_comment_template.md`, 'utf8');
 console.log("Template file contents:");
 console.log(template);
 console.log(`Issue number: ${github.context.issue.number}`);
 
+const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
+
+// octokit.rest.issues.createComment();
 // await octokit.rest.issues.createComment({
 //     repo: process.env.GITHUB_REPOSITORY,
 //     owner: process.env.GITHUB_REPOSITORY_OWNER,
