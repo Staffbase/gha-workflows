e4f4ad2797ab73cd23d04fe883ffc1d895a8385d
Jan Reimann <jan.reimann@staffbase.com>
2023-06-23 12:00:16 +0200

    chore(testio): read template file and print to consolse

--- .github/scripts/testio/addPRcomment.ts
+++ .github/scripts/testio/addPRcomment.ts
@@ -3,28 +3,11 @@ import * as github from "@actions/github";
 import * as fs from "fs";
 import * as path from "path";
 
-const octokit = new Octokit({
-    auth: process.env.GITHUB_TOKEN
-})
+const octokit: Octokit = github.getOctokit(process.env.GITHUB_TOKEN);
 
-console.log("List of all env vars:");
-console.log(process.env);
-
-console.log("List Github context:");
-console.log(github.context);
-
-console.log("List Github issue number:");
-console.log(github.context.issue.number);
-
-console.log("List Github repo owner:");
-console.log(github.context.repo.owner);
-
-console.log("List Github repo:");
-console.log(github.context.repo.repo);
-
-// const template = fs.readFileSync(path.join(process.env.GITHUB_WORKSPACE, '.github/exploratory_test_template.md'), 'utf8');
-// console.log("Template file contents:");
-// console.log(template);
+const template = fs.readFileSync(path.join(process.env.GITHUB_WORKSPACE, `${process.env.TESTIO_SCRIPTS_DIR}/exploratory_test_comment_template.md`), 'utf8');
+console.log("Template file contents:");
+console.log(template);
 
 // await octokit.rest.issues.createComment({
 //     repo: process.env.GITHUB_REPOSITORY,
