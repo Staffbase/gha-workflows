b5a7aa832d533d520c03afde5ef52b8df1e6e715
Jan Reimann <jan.reimann@staffbase.com>
2023-06-23 12:35:26 +0200

    chore(testio): asynchronize

--- .github/scripts/testio/addPRcomment.ts
+++ .github/scripts/testio/addPRcomment.ts
@@ -2,21 +2,25 @@ import * as github from "@actions/github";
 import * as fs from "fs";
 import {Octokit} from "@octokit/rest";
 
-const template = fs.readFileSync(`${process.env.TESTIO_SCRIPTS_DIR}/exploratory_test_comment_template.md`, 'utf8');
-console.log("Template file contents:");
-console.log(template);
-console.log(`Issue number: ${github.context.issue.number}`);
+async function addComment() {
+    const template = fs.readFileSync(`${process.env.TESTIO_SCRIPTS_DIR}/exploratory_test_comment_template.md`, 'utf8');
+    console.log("Template file contents:");
+    console.log(template);
+    console.log(`Issue number: ${github.context.issue.number}`);
 
 // const octokit = github.getOctokit(`${process.env.GITHUB_TOKEN}`);
 
-const octokit = new Octokit({
-    auth: process.env.GITHUB_TOKEN
-});
+    const octokit = new Octokit({
+        auth: process.env.GITHUB_TOKEN
+    });
 
-await octokit.rest.issues.createComment({
-     repo: github.context.repo.repo,
-     owner: github.context.repo.owner,
-     issue_number: github.context.issue.number,
-     body: template,
-});
+    await octokit.rest.issues.createComment({
+        repo: github.context.repo.repo,
+        owner: github.context.repo.owner,
+        issue_number: github.context.issue.number,
+        body: template,
+    });
 
+}
+
+addComment().then();
