d45cd076a77e0d79eedb43a0430f12911cde4363
Jan Reimann <jan.reimann@staffbase.com>
2023-06-23 12:32:23 +0200

    chore(testio): first attempt to add comment

--- .github/scripts/testio/addPRcomment.ts
+++ .github/scripts/testio/addPRcomment.ts
@@ -1,18 +1,22 @@
 import * as github from "@actions/github";
 import * as fs from "fs";
+import {Octokit} from "@octokit/rest";
 
 const template = fs.readFileSync(`${process.env.TESTIO_SCRIPTS_DIR}/exploratory_test_comment_template.md`, 'utf8');
 console.log("Template file contents:");
 console.log(template);
 console.log(`Issue number: ${github.context.issue.number}`);
 
-const octokit = github.getOctokit(`${process.env.GITHUB_TOKEN}`);
+// const octokit = github.getOctokit(`${process.env.GITHUB_TOKEN}`);
 
-// octokit.rest.issues.createComment();
-// await octokit.rest.issues.createComment({
-//     repo: process.env.GITHUB_REPOSITORY,
-//     owner: process.env.GITHUB_REPOSITORY_OWNER,
-//     issue_number: parseInt(process.env.GITHUB_ISSUE_NUMBER),
-//     body: template,
-// });
+const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN
+});
+
+await octokit.rest.issues.createComment({
+     repo: github.context.repo.repo,
+     owner: github.context.repo.owner,
+     issue_number: github.context.issue.number,
+     body: template,
+});
 
