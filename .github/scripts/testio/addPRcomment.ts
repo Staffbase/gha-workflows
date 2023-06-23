7cc9538aa871893e346aa6bb2bd65272068ab7d4
Jan Reimann <jan.reimann@staffbase.com>
2023-06-23 12:08:59 +0200

    chore(testio): instantiate Octokit by calling constructor

--- .github/scripts/testio/addPRcomment.ts
+++ .github/scripts/testio/addPRcomment.ts
@@ -3,11 +3,14 @@ import * as github from "@actions/github";
 import * as fs from "fs";
 import * as path from "path";
 
-const octokit: Octokit = github.getOctokit(process.env.GITHUB_TOKEN);
+const octokit: Octokit = new Octokit({
+   auth: process.env.GITHUB_TOKEN
+});
 
 const template = fs.readFileSync(path.join(process.env.GITHUB_WORKSPACE, `${process.env.TESTIO_SCRIPTS_DIR}/exploratory_test_comment_template.md`), 'utf8');
 console.log("Template file contents:");
 console.log(template);
+console.log(`Issue number: ${github.context.issue.number}`);
 
 // await octokit.rest.issues.createComment({
 //     repo: process.env.GITHUB_REPOSITORY,
