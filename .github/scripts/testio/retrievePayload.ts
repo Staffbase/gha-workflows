02e91af16ad1f8246474bf2426dfb057bbfab4a8
Jan Reimann <jan.reimann@staffbase.com>
2023-06-26 10:44:05 +0200

    chore(testio): introduce JSON schema

--- .github/scripts/testio/retrievePayload.ts
+++ .github/scripts/testio/retrievePayload.ts
@@ -2,9 +2,11 @@ import * as github from "@actions/github";
 import * as fs from "fs";
 import {Octokit} from "@octokit/rest";
 import * as core from "@actions/core";
+import Ajv from "ajv";
 
 async function createPayload() {
     const commentID: number = Number(process.env.TESTIO_SUBMIT_COMMENT_ID);
+    const commentUrl = `${process.env.TESTIO_SUBMIT_COMMENT_URL}`;
 
     const octokit = new Octokit({
         auth: process.env.GITHUB_TOKEN
@@ -16,9 +18,21 @@ async function createPayload() {
         comment_id: commentID
     });
 
-    console.log("This comment has been received:");
-    console.log(retrievedComment);
+    const commentContents = retrievedComment.body;
+    if (!commentContents) throw new Error(`Comment ${commentUrl} seems to be empty`);
 
+    const jsonRegex = /```json\s(.+)\s```/sm;       // everything between ```json and ``` so that we can parse it
+    const matches = jsonRegex.exec(commentContents);
+    const jsonContents = matches[1];                // 0 index = full match, 1 index = 1st capture group
+    if (!jsonContents) throw new Error("Provided input seems to be empty between ```json and ```");
+    const preparation = JSON.parse(jsonContents);
+
+    const prepareTestSchemaFile = `${process.env.TESTIO_SCRIPTS_DIR}/exploratory_test_comment_prepare_schema.json`;
+    const prepareTestSchema = JSON.parse(fs.readFileSync(prepareTestSchemaFile, 'utf8'));
+    const ajv = new Ajv();
+    const validation = ajv.compile(prepareTestSchema);
+    const valid = validation(preparation);
+    if (!valid) throw new Error(`Provided json is not conform to schema: ${validation.errors}`);
 }
 
 createPayload().then();
