b71185de9fcb10ed85aeae0faa625fa70212b3f1
Jan Reimann <jan.reimann@staffbase.com>
2023-06-27 14:21:45 +0200

    chore(testio): commented code

--- .github/scripts/testio/retrievePayload.ts
+++ .github/scripts/testio/retrievePayload.ts
@@ -23,36 +23,36 @@ async function createPayload() {
     console.log("retrieved comment:");
     console.log(retrievedComment);
 
-    const commentContents: string = retrievedComment.data.body;
-    if (!commentContents) Util.throwErrorAndPrepareErrorMessage(`Comment ${commentUrl} seems to be empty`, errorFileName);
-
-    const jsonRegex = /```json\s(.+)\s```/sm;       // everything between ```json and ``` so that we can parse it
-    let preparation;
-    try {
-        preparation = Util.getJsonObjectFromComment(jsonRegex, commentContents, 1);
-    } catch (error) {
-        Util.throwErrorAndPrepareErrorMessage(error, errorFileName);
-    }
-
-    const prepareTestSchemaFile = `${process.env.TESTIO_SCRIPTS_DIR}/exploratory_test_comment_prepare_schema.json`;
-    const {valid, validation} = Util.validateObjectAgainstSchema(preparation, prepareTestSchemaFile);
-    if (!valid) {
-        if (validation.errors) {
-            const output = betterAjvErrors(prepareTestSchemaFile, preparation, validation.errors);
-            console.log(output);
-            Util.throwErrorAndPrepareErrorMessage(`Provided json is not conform to schema: ${output}`, errorFileName);
-        }
-        Util.throwErrorAndPrepareErrorMessage("Provided json is not conform to schema", errorFileName);
-    }
-
-    const testIOPayload = Util.convertPrepareObjectToTestIOPayload(preparation, github.context.repo.repo, github.context.repo.owner, github.context.issue.number);
-    console.log("Converted payload:");
-    console.log(testIOPayload);
-    const payloadFile = `${process.env.TESTIO_SCRIPTS_DIR}/testio_payload.json`;
-    await fs.writeFile(payloadFile, JSON.stringify(testIOPayload), (err) => {
-        if (err) Util.throwErrorAndPrepareErrorMessage(err.message, errorFileName);
-        console.log(`The payload file ${payloadFile} has been saved successfully`);
-    });
+    // const commentContents: string = retrievedComment.data.body;
+    // if (!commentContents) Util.throwErrorAndPrepareErrorMessage(`Comment ${commentUrl} seems to be empty`, errorFileName);
+    //
+    // const jsonRegex = /```json\s(.+)\s```/sm;       // everything between ```json and ``` so that we can parse it
+    // let preparation;
+    // try {
+    //     preparation = Util.getJsonObjectFromComment(jsonRegex, commentContents, 1);
+    // } catch (error) {
+    //     Util.throwErrorAndPrepareErrorMessage(error, errorFileName);
+    // }
+    //
+    // const prepareTestSchemaFile = `${process.env.TESTIO_SCRIPTS_DIR}/exploratory_test_comment_prepare_schema.json`;
+    // const {valid, validation} = Util.validateObjectAgainstSchema(preparation, prepareTestSchemaFile);
+    // if (!valid) {
+    //     if (validation.errors) {
+    //         const output = betterAjvErrors(prepareTestSchemaFile, preparation, validation.errors);
+    //         console.log(output);
+    //         Util.throwErrorAndPrepareErrorMessage(`Provided json is not conform to schema: ${output}`, errorFileName);
+    //     }
+    //     Util.throwErrorAndPrepareErrorMessage("Provided json is not conform to schema", errorFileName);
+    // }
+    //
+    // const testIOPayload = Util.convertPrepareObjectToTestIOPayload(preparation, github.context.repo.repo, github.context.repo.owner, github.context.issue.number);
+    // console.log("Converted payload:");
+    // console.log(testIOPayload);
+    // const payloadFile = `${process.env.TESTIO_SCRIPTS_DIR}/testio_payload.json`;
+    // await fs.writeFile(payloadFile, JSON.stringify(testIOPayload), (err) => {
+    //     if (err) Util.throwErrorAndPrepareErrorMessage(err.message, errorFileName);
+    //     console.log(`The payload file ${payloadFile} has been saved successfully`);
+    // });
 }
 
 createPayload().then();
