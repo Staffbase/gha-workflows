4f268761589f77a2ea3ae800f09f686d793af982
Jan Reimann <jan.reimann@staffbase.com>
2023-07-04 09:49:40 +0200

    chore: use testio-trigger-test-github-action and remove implementation

--- .github/scripts/testio/exploratory_test_comment_prepare_template.md
+++
@@ -1,6 +0,0 @@
-
-@bot-testio exploratory-test preparation
-```json
-$$REQUIRED_INFORMATION_TEMPLATE$$
-```
-As response to [test creation trigger]($$CREATE_COMMENT_URL$$).
--- .github/scripts/testio/jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  collectCoverage: false,
-  // coverageReporters: ["clover", "json", "lcov", "text", "html"],
-  // testTimeout: 10 * 1000,
-};
